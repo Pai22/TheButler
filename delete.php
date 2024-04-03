7484e68ad2e81ad74806fcec7e0c55e9747b74ad
Pai22 <paisrp22@gmail.com>
2024-04-03 20:36:00 +0700

    chang 555

--- delete.php
+++ delete.php
@@ -1,11 +1,9 @@
 <?php 
     session_start();
-    if(isset($_SESSION['role']) && ($_SESSION['role'] == 'a' || $_SESSION['role'] == 'm')){
-        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-        $sql = "DELETE FROM post WHERE id = $_GET[id]";
-        $conn->exec($sql);//ถ้าลบตารางจะใช้ "exec"
-        $sql = "DELETE FROM comment WHERE post_id = $_GET[id]";
-        $conn->exec($sql);//ถ้า select ตารางจะใช้ "query"
+    if(isset($_SESSION['role']) && ($_SESSION['role'] == 'a' )){
+        $conn = new PDO("mysql:host=localhost;dbname=butler;charset=utf8","root","");
+        $sql = "DELETE FROM community_admin WHERE id = $_GET[id]";
+        $conn->exec($sql);//ถ้าลบตารางจะใช้ "exec"  ถ้า select ตารางจะใช้ "query"
         $conn = null;//สั่งปิดการเชื่อมต่อ
         header("location:index.php");      
         die();
--- newpost_save.php
+++ newpost_save.php
@@ -3,12 +3,11 @@
 session_start();
     $title = $_POST['topic'];
     $content = $_POST['content'];
-    $cat_id = $_POST['category'];
     $user_id = $_SESSION['user_id'];
 
-    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $conn = new PDO("mysql:host=localhost;dbname=butler;charset=utf8","root","");
 
-    $sql = "INSERT INTO post (title, content, post_date, cat_id, user_id) VALUES ('$title', ' $content',NOW(),'$cat_id','$user_id')";
+    $sql = "INSERT INTO community_admin (title, content, post_date, user_id) VALUES ('$title', ' $content',NOW(),'$user_id')";
     $conn->exec($sql);
     $conn = null;
     header("location:index.php");
