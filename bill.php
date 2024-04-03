6896be8d4ea7c2aeac4fcae3100e943647934f63
Nongmimi <worawaran.ejk@gmail.com>
2024-04-03 20:49:14 +0700

    no database

--- bill.php
+++ bill.php
@@ -1,9 +1,5 @@
 <?php session_start();
-if (isset($_GET['name'])) {
-    $catname = $_GET['name'];
-} else {
-    $catname = '--ทั้งหมด--';
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,47 +15,19 @@ if (isset($_GET['name'])) {
 
 <body style="background-color: black;">
         <?php include "nav.php" ?>
-        <div class="container" style="margin-top: 50px;">
-            <div class="card bg-dark">
-                <div class="card-header">
-                    <ul class="nav nav-tabs card-header-tabs text-w">
-                        <li class="nave-item"><a href="#" class="nav-link">ค่าน้ำ</a></li>
-                        <li class="nave-item"><a href="#" class="nav-link">ค่าไฟ</a></li>
-                        <li class="nave-item"><a href="#" class="nav-link">ค่าห้อง</a></li>
-                        <li class="nave-item"><a href="#" class="nav-link">รวม</a></li>
-                    </ul>
-                </div>
+        <div class="container mt-5">
+            <div class="card">
+            <div class="card-body">
+                <h4 class="card-text">ห้อง $row[1]</h4>
+                <br>
+                <h5 class="card-text">ค่าน้ำ : <input type= text name= username required class=form-control></h5>
+                <h5 class="card-text">ค่าไฟ: <input type= text name= username required class=form-control></h5>
+                <h5 class="card-text">ค่าห้อง: <input type= text name= username required class=form-control></h5>
+                <h5 class="card-text">รวมทั้งหมด: <input type= text name= username required class=form-control disabled></h5>
+            </div>
+            <button type="button" class = 'btn btn-success mt-1'>confirm</button>
             </div>
         </div>
-        <span>
-            <div class="mt-5 mb-3 d-flex justify-content-between " style="float:left ;">
-                <h6 style="color: white;"><i class="bi bi-building-fill"></i> Total Rooms</h6>
-            </div>
-                <table class="table">
-                    <tbody>
-                        <tr class = "table-danger"><td> Room : </td>
-                            <td>
-                                <button type="button" class="btn btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-receipt text-danger" style = "font-size:20px"></i></button>
-                                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Receipt</h1>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                        <div class="modal-body">
-                                            <img src="bill.jpg" class="img-fluid" id="image">
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        </div>
-                                    </div>
-                                    </div>
-                                <button type="button" class = 'btn btn-danger btn-sm mt-1'>confirm</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-        </span>
+        
 </body>
 </html>
