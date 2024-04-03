39f188973bc574091442b4cfc0a1a92fa29b23b8
Nongmimi <worawaran.ejk@gmail.com>
2024-04-03 23:51:13 +0700

    complete

--- bill.php
+++ bill.php
@@ -14,53 +14,54 @@
 </head>
 
 <body style="background-color: black;">
-        <?php include "nav.php" ?>
+    <?php include "nav.php" ?>
         <div class="container mt-5">
-        <div class="row justify-content-center">
-        <div class="col-md-5">
-            <div class="card" style="height:520px;width:500px">
-            <div class="card-body">
-                <br>
-                <?php
-            $conn = new PDO("mysql:host=localhost;dbname=butler;charset=utf8", "root", "");
-            $sql="select username from user";
-            $result = $conn->query($sql);
-            $i=0;
-            while ($row = $result->fetch()) {
-                $i+=1;
-                if ($_SESSION['role'] == 'a') {
-                    echo "<tr><td>Room : $row[0]";
-                    echo "</td></tr>";}}?>
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="card" style="height:520px;width:500px">
+                        <div class="card-body">
+                            <?php
+                                $conn = new PDO("mysql:host=localhost;dbname=butler;charset=utf8", "root", "");
+                                $sql="select username,user_id from user";
+                                $result = $conn->query($sql);
+                                $i=0;
+                                while ($row = $result->fetch()) {
+                                    $i+=1;
+                                    if(isset($_SESSION['id']) && ($_SESSION['role'] == 'a')){
+                                        if($row['user_id']== $i){
+                                            echo "<h4>Room : $row[0]</h4><br>";
+                                        }
+                                    }
+                                }
+                            ?>
 
-                <h5 class="card-text">น้ำใช้ไป :</h5><input type= text id="num1" required class=form-control style="height:30px;width:150px" placeholder="ตอบเป็นหน่วย"><br>
-                <h5 class="card-text">ไฟใช้ไป:</h5><input type= text id="num2" required class=form-control style="height:30px;width:150px" placeholder="ตอบเป็นหน่วย"><br>
-                <h5 class="card-text">ค่าห้อง:</h5><input type= text id="num3" value="4000" class=form-control disabled style="height:30px;width:100px"><br>
-                <button onclick="calculate()">คำนวณ</button><br>
+                            <h5 class="card-text">น้ำใช้ไป :</h5><input type= text id="num1" required class=form-control style="height:30px;width:150px" placeholder="ตอบเป็นหน่วย"><br>
+                            <h5 class="card-text">ไฟใช้ไป:</h5><input type= text id="num2" required class=form-control style="height:30px;width:150px" placeholder="ตอบเป็นหน่วย"><br>
+                            <h5 class="card-text">ค่าห้อง:</h5><input type= text id="num3" value="4000" class=form-control disabled style="height:30px;width:100px"><br>
+                            <button onclick="calculate()">คำนวณ</button><br>
                 
-            
-            <form action="bill_save.php" method="post">
-                <p id="result"><h5 class="card-text">รวมทั้งหมด:</h5><input type= text id="price" name="price" required class=form-control style="height:30px;width:100px" oninput="calculate(this)"></p>
-                <button type="submit" class = 'btn btn-success mt-1'>บันทึก</button>
-            </form>
-        </div>
+                            <form action="bill_save.php" method="post">
+                                <p id="result"><h5 class="card-text">รวมทั้งหมด:</h5><input type= text id="price" name="price" required class=form-control style="height:30px;width:100px" oninput="calculate(this)"></p>
+                                <button type="submit" class = 'btn btn-success mt-1'>บันทึก</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
-        </div>
         <script>
-        function calculate(input) {
+            function calculate(input) {
             // รับค่าจากช่อง input
-            var num1 = document.getElementById("num1").value;
-            var num2 = document.getElementById("num2").value;
-            var num3 = document.getElementById("num3").value;
+                var num1 = document.getElementById("num1").value;
+                var num2 = document.getElementById("num2").value;
+                var num3 = document.getElementById("num3").value;
 
-            // ทำการคำนวณ
-            var price = (parseFloat(num1)*18) + (parseFloat(num2)*8) + (parseFloat(num3));
+                // ทำการคำนวณ
+                var price = (parseFloat(num1)*18) + (parseFloat(num2)*8) + (parseFloat(num3));
 
             // แสดงผลลัพธ์
-            document.getElementById("price").value = price;
-        }
-    </script>
-        
-</body>
+                document.getElementById("price").value = price;
+            }
+        </script>  
+    </body>
 </html>
--- room.php
+++ room.php
@@ -16,11 +16,11 @@ if (isset($_GET['name'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script>
-function showPopup() {
-  // เปิดหน้าต่าง pop-up ด้วยความกว้าง 500px และความสูง 500px
-  window.open('popup_image.php', 'popup', 'width=500,height=500');
-}
-</script>
+        function showPopup() {
+            // เปิดหน้าต่าง pop-up ด้วยความกว้าง 500px และความสูง 500px
+            window.open('popup_image.php', 'popup', 'width=500,height=500');
+        }
+    </script>
 </head>
 
 <body style="background-color: black;">
@@ -32,27 +32,26 @@ function showPopup() {
                 <table class="table">
             <?php
             $conn = new PDO("mysql:host=localhost;dbname=butler;charset=utf8", "root", "");
-            $sql="select id,username,name,gender,email,image from user";
+            $sql="select id,number_room,username,name,gender,email,image,role from user";
             $result = $conn->query($sql);
-            $i=0;
+            $i=1;
             while ($row = $result->fetch()) {
-                $i+=1;
-                if ($_SESSION['role'] == 'a'){
+                if(isset($_SESSION['id']) && ($_SESSION['role'] == 'a')){
+                    if($row[7]=='m'){
                     echo "<tr><td><a href=bill.php?$i style = text-decoration:none>Room : $row[1]</a>";
                     echo "<td>";
-                
                     if (isset($_SESSION['id']) && ($_SESSION['role'] == 'a')) {
                         echo "<td>";
                         echo "<button onclick=showPopup()>
                         <i class='bi bi-receipt'></i></a></td>";
-                        echo "<td>";
-                        echo "<button onclick=confirm()>
+                                    echo "<td>";
+                                    echo "<button onclick=confirm()>
                         confirm";
-                            
+                $i+=1;
+                    }
             }}
         }
             $conn = null;
-            
             ?>
             
         </table>
